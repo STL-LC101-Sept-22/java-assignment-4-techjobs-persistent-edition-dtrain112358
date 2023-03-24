a41f7961789f389875c9dc4053c10ad6bf3de2c9
Dtrain112358 <dtrain11235813@protonmail.com>
2023-03-23 21:40:58 -0500

    made both Entity and Respository Annotations to Employer and Skill Classes and respective Repos, added public accesor methods to Employer and Skill Classes respectively

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,31 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+@Entity
 public class Employer extends AbstractEntity {
+    @NotBlank
+    @Size(min = 3, max = 50, message = "Location should be 3-50 Characters long")
+    private String location;
+
+// TODO #1: Add public accessor methods to Employer
+
+
+    public Employer (String location) {
+        this.location = location;
+    }
+
+    public Employer () {}
+
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
 
 }
