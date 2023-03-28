a80e0fe12e44c4575acd8b58adb1976a5bb86ded
Dtrain112358 <dtrain11235813@protonmail.com>
2023-03-27 20:53:33 -0500

    added all the completed things for part 3 and part 4

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,7 +1,12 @@
 package org.launchcode.techjobs.persistent.controllers;
 
+import org.apache.catalina.Store;
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.Skill;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
@@ -18,6 +24,12 @@ import java.util.List;
 public class HomeController {
     @Autowired
     private EmployerRepository employerRepository;
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
+    private SkillRepository skillRepository;
 
     @RequestMapping("")
     public String index(Model model) {
@@ -31,6 +43,7 @@ public class HomeController {
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
         model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("skills", skillRepository.findAll());
         model.addAttribute(new Job());
         return "add";
     }
@@ -43,11 +56,15 @@ public class HomeController {
             model.addAttribute("title", "Add Job");
             return "add";
         } else {
-            // identify employer object
-            //
-            employerRepository.findById(employerId);
-
+            Optional<Employer> optEmployer = employerRepository.findById(employerId);
+            if (optEmployer.isPresent()) {
+                newJob.setEmployer(optEmployer.get());
+            }
+            List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
+            newJob.setSkills(skillObjs);
         }
+
+        jobRepository.save(newJob);
         return "redirect:";
     }
 
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,12 +1,17 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
 
     private String description;
 
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<> ();
 
     public Skill (String description) {
         this.description = description;
@@ -22,5 +27,11 @@ public class Skill extends AbstractEntity {
         this.description = description;
     }
 
+    public List<Job> getJobs() {
+        return jobs;
+    }
 
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 }
